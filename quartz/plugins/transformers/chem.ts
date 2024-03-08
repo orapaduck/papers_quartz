782fd5b4d1b2dc2f03756937eaa7fae71b2bd7a3
orapaduck <jihuno0803@gmail.com>
2024-03-09 00:53:30 +0900

    modified:   quartz/plugins/transformers/chem.ts

--- quartz/plugins/transformers/chem.ts
+++ quartz/plugins/transformers/chem.ts
@@ -1,19 +1,17 @@
 import { QuartzTransformerPlugin } from "../types";
-import OCL from 'openchemlib';
-
-const SmilesRenderer: QuartzTransformerPlugin = () => {
+import { Molecule } from 'openchemlib';
+import { visit } from "unist-util-visit"
+export const SmilesRenderer: QuartzTransformerPlugin = () => {
   return {
     name: "SmilesRenderer",
     markdownPlugins() {
       return [
         () => {
           return (tree, file) => {
-            const visit = require('unist-util-visit');
-            
             visit(tree, 'code', (node) => {
               if (node.lang === 'smiles') {
                 const smiles = node.value;
-                const molecule = OCL.Molecule.fromSmiles(smiles);
+                const molecule = Molecule.fromSmiles(smiles);
                 const svg = molecule.toSVG(300, 300);
                 node.type = 'html';
                 node.value = `<div class="smiles-container">${svg}</div>`;
@@ -25,5 +23,3 @@ const SmilesRenderer: QuartzTransformerPlugin = () => {
     },
   };
 }
-
-export default SmilesRenderer;
