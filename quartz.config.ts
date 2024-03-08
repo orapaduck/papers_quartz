92f92212707a68051aff63663a3d9c31d864d96c
orapaduck <jihuno0803@gmail.com>
2024-03-08 14:47:13 +0900

    modified:   quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -52,7 +52,7 @@ const config: QuartzConfig = {
         // if you do rely on git for dates, ensure defaultDateType is 'modified'
         priority: ["frontmatter", "filesystem"],
       }),
-      Plugin.SmilesRender(),
+      Plugin.RenderSmiles(),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
