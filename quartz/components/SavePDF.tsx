cc6f3478998ac35b726b3e5b08c5d61dfa53926d
cld338 <jihuno291@gmail.com>
2024-03-11 00:12:23 +0900

    modified:   quartz/components/SavePDF.tsx

--- quartz/components/SavePDF.tsx
+++ quartz/components/SavePDF.tsx
@@ -3,15 +3,15 @@ import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 
 export default (() => {
-  function downloadPDF() {
+  const downloadPDF = () => {
     const saveAsPdf = async () => {
       const canvas = await html2canvas(document.body);
       const imgData = canvas.toDataURL('image/png');
       const pdf = new jsPDF();
-      var imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
-      var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
-      var imgHeight = canvas.height * imgWidth / canvas.width;
-      var heightLeft = imgHeight;
+      const imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
+      const pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const heightLeft = imgHeight;
       pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
       pdf.save('download.pdf');
     };
@@ -19,7 +19,7 @@ export default (() => {
     return (
       <button onClick={saveAsPdf}>Save as PDF</button>
     );
-  }
+  };
 
   return downloadPDF;
-}) satisfies QuartzComponentConstructor;
+}) as QuartzComponentConstructor;
