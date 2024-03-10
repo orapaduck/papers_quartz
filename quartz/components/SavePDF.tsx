afb36b088d677197b7de67433da5daf2a4308173
cld338 <jihuno291@gmail.com>
2024-03-11 00:21:05 +0900

    modified:   quartz/components/SavePDF.tsx

--- quartz/components/SavePDF.tsx
+++ quartz/components/SavePDF.tsx
@@ -1,20 +1,22 @@
-import { QuartzComponentConstructor } from "./types";
+import { QuartzComponentConstructor } from "./types"
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 
 export default (() => {
-  const saveAsPdf = async () => {
-    const canvas = await html2canvas(document.body);
-    const imgData = canvas.toDataURL('image/png');
-    const pdf = new jsPDF();
-    const imgWidth = 190; // A4 width in mm
-    const imgHeight = canvas.height * imgWidth / canvas.width;
-    const heightLeft = imgHeight;
-    pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
-    pdf.save('download.pdf');
-  };
-
   function downloadPDF() {
+    const saveAsPdf = () => {
+      html2canvas(document.body).then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        const imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
+        const pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        const heightLeft = imgHeight;
+        pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+        pdf.save('download.pdf');
+      });
+    };
+
     return (
       <button onClick={saveAsPdf}>Save as PDF</button>
     );
