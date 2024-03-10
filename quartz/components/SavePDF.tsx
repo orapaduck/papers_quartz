ef30651a959c860078d7fa252248a5a3d64fcbb1
cld338 <jihuno291@gmail.com>
2024-03-10 23:55:51 +0900

    modified:   quartz/components/SavePDF.tsx

--- quartz/components/SavePDF.tsx
+++ quartz/components/SavePDF.tsx
@@ -8,8 +8,12 @@ export const SaveToPDFComponent: QuartzComponent = (props: QuartzComponentProps)
     if (element) {
       const canvas = await html2canvas(element);
       const imageData = canvas.toDataURL('image/png');
+      var imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
+	    var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+	    var imgHeight = canvas.height * imgWidth / canvas.width;
+	    var heightLeft = imgHeight;
       const pdf = new jsPDF();
-      pdf.addImage(imageData, 'PNG', 0, 0);
+      pdf.addImage(imageData, 'PNG', 0, 0, imgWidth, imgHeight);
       pdf.save('download.pdf');
     }
   };
