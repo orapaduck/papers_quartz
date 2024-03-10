18debcc654d64c98a20ad9e5f545322d0c68536f
cld338 <jihuno291@gmail.com>
2024-03-11 00:06:36 +0900

    modified:   quartz/components/SavePDF.tsx

--- quartz/components/SavePDF.tsx
+++ quartz/components/SavePDF.tsx
@@ -1,26 +1,25 @@
-import { QuartzComponent, QuartzComponentProps } from "./types";
+import { QuartzComponentConstructor } from "./types"
 import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import { jsPDF } from 'jspdf';
 
-export const SaveToPDFComponent: QuartzComponent = (props: QuartzComponentProps) => {
-  const downloadPDF = async () => {
-    const element = document.getElementById('quartz-root');
-    if (element) {
-      const canvas = await html2canvas(element);
-      const imageData = canvas.toDataURL('image/png');
-      var imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
-	    var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
-	    var imgHeight = canvas.height * imgWidth / canvas.width;
-	    var heightLeft = imgHeight;
+export default (() => {
+  function downloadPDF() {
+    const saveAsPdf = async () => {
+      const canvas = await html2canvas(document.body);
+      const imgData = canvas.toDataURL('image/png');
       const pdf = new jsPDF();
-      pdf.addImage(imageData, 'PNG', 0, 0, imgWidth, imgHeight);
+      var imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
+      var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
       pdf.save('download.pdf');
-    }
-  };
+    };
 
-  return (
-    <button onClick={downloadPDF}>
-      Save as PDF
-    </button>
-  );
-};
+    return (
+      <button onClick={saveAsPdf}>Save as PDF</button>
+    );
+  }
+
+  return downloadPDF;
+}) satisfies QuartzComponentConstructor;
