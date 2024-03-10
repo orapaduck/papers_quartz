c544987ad3d28a613f496d9b9677a9ad5180fb3e
cld338 <jihuno291@gmail.com>
2024-03-10 23:51:56 +0900

    modified:   quartz.layout.ts 	modified:   quartz/components/SavePDF.tsx 	modified:   quartz/components/index.ts

--- quartz/components/SavePDF.tsx
+++ quartz/components/SavePDF.tsx
@@ -1,31 +1,22 @@
-import { QuartzComponentConstructor, QuartzComponentProps } from "./types";
-import style from "./styles/pdfExporter.scss";
-import * as html2pdf from 'html2pdf.js';
-interface PdfExporterOptions {
-  fileName: string;
-}
-export default ((opts?: PdfExporterOptions) => {
-  function PdfExporter({ displayClass, cfg }: QuartzComponentProps) {
-    const exportPDF = () => {
-      const element = document.body; // or any specific element you want to export
-      const options = {
-        margin:       10,
-        filename:     opts?.fileName || 'document.pdf',
-        image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
-      };
+import { QuartzComponent, QuartzComponentProps } from "./types";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
-      html2pdf().from(element).set(options).save();
+export const SaveToPDFComponent: QuartzComponent = (props: QuartzComponentProps) => {
+  const downloadPDF = async () => {
+    const element = document.getElementById('quartz-root');
+    if (element) {
+      const canvas = await html2canvas(element);
+      const imageData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      pdf.addImage(imageData, 'PNG', 0, 0);
+      pdf.save('download.pdf');
     }
+  };
 
-    return (
-      <div class={`${displayClass ?? ""}`}>
-        <button onClick={exportPDF} class="pdf-export-button">Export as PDF</button>
-      </div>
-    )
-  }
-
-  PdfExporter.css = style;
-  return PdfExporter;
-}) satisfies QuartzComponentConstructor;
+  return (
+    <button onClick={downloadPDF}>
+      Save as PDF
+    </button>
+  );
+};
--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -19,7 +19,7 @@ import DesktopOnly from "./DesktopOnly"
 import MobileOnly from "./MobileOnly"
 import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
-import PdfExporter from "./SavePDF"
+import downloadPDF from "./SavePDF"
 
 export {
   ArticleTitle,
@@ -43,5 +43,5 @@ export {
   RecentNotes,
   NotFound,
   Breadcrumbs,
-  PdfExporter,
+  downloadPDF,
 }
